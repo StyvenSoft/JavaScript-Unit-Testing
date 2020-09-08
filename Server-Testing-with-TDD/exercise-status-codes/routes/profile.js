89f8e800ef9b0a52787a47dc9a5af247f3e44abb
Steveen <steveen.echeverri@gmail.com>
2020-09-08 18:50:55 -0500

    Refactoring: Handlebars profile template

--- Server-Testing-with-TDD/exercise-status-codes/routes/profile.js
+++ Server-Testing-with-TDD/exercise-status-codes/routes/profile.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 
 router.get('/:username', (req, res) => {
-    res.send('<h1 id="welcome-message">Welcome ' + req.params.username + '!</h1>');
+    // res.send('<h1 id="welcome-message">Welcome ' + req.params.username + '!</h1>');
+    const username = req.params.username;
+    res.render('profile', {username});
 });
 
 router.get('/:username', (req, res) => {
