b2b87e4033d6ff16a682ecf5ddf482405d3667d5
Steveen <steveen.echeverri@gmail.com>
2020-09-11 18:46:33 -0500

    Created new describe test for dino

--- Server-Testing-with-TDD/exercise-model-testing/test/models/dinosaur-test.js
+++ Server-Testing-with-TDD/exercise-model-testing/test/models/dinosaur-test.js
@@ -4,6 +4,23 @@ const { mongoose, databaseUrl, options } = require('../../database');
 
 describe('Dinosaur', () => {
 
+    describe('#save', () => {
+        it('persists a dino', async () => {
+            const fields = {
+                name: 'Velociraptor',
+                count: 3,
+                risk: 'High'
+            };
+            const dino = new Dinosaur(fields);
+
+            await dino.save();
+            const stored = await Dinosaur.find({ 'name': 'Velociraptor' });
+
+            assert.strictEqual(stored.length, 1);
+            assert.include(stored[0], fields);
+        });
+    });
+
     describe('#name', () => {
         it('is a String', () => {
             const dino = new Dinosaur({ name: 'T-rex' });
