d94f7ccb9bb15070f401ec64e670138a71a53ede
Steveen <steveen.echeverri@gmail.com>
2020-09-11 19:44:53 -0500

    beforeEach hook after the call to connect

--- Server-Testing-with-TDD/exercise-model-testing/test/models/dinosaur-test.js
+++ Server-Testing-with-TDD/exercise-model-testing/test/models/dinosaur-test.js
@@ -1,8 +1,17 @@
 const Dinosaur = require('../../models/dinosaur');
 const { assert } = require('chai');
-const { mongoose, databaseUrl, options } = require('../../database');
+const { connectAndDrop, disconnect } = require('../../database');
 
 describe('Dinosaur', () => {
+
+    beforeEach(async () => {
+        await mongoose.connect(databaseUrl, options);
+        await mongoose.connection.db.dropDatabase();
+    });
+
+    afterEach(async () => {
+        await mongoose.disconnect;
+    });
 
     describe('#save', () => {
         it('persists a dino', async () => {
