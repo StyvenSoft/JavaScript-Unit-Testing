410052517c7b358ca17654cffbfd4973dfedf4e9
Steveen <steveen.echeverri@gmail.com>
2020-09-14 16:19:36 -0500

    Red at Server and Model

--- Server-Testing-with-TDD/exercise-model-layer/models/quotes.js
+++ Server-Testing-with-TDD/exercise-model-layer/models/quotes.js
@@ -1,1 +1,10 @@
 // models/quote.js
+const { mongoose } = require('../database');
+
+const quoteSchema = new mongoose.Schema({
+    quote: String
+});
+
+const Quote = mongoose.model('Quote', quoteSchema);
+
+module.exports = Quote;
